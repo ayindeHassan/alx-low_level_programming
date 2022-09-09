1e5aff85375631a7444d02019db8f9bfbe12af44
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-09 14:52:07 -0700

    0-9 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
- int num;
- for (num = 0; num <= 9; num++)
- {
- putchar(num);
- }
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num);
+}
 
- putchar('\n');
- return (0);
+putchar('\n');
+return (0);
 }
