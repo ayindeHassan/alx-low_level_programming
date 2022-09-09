e75354994f1baef2a24ee9687485888a6566fb3e
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-09 16:28:45 -0700

    advance way of using ,

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-int num1, nuum2;
+int num1, num2;
 for (num1 = 0; num1 < 9; num1++)
 {
 for (num2 = num1 + 1; num2 < 10; num2++)
@@ -19,7 +19,7 @@ putchar((num2 % 10) + '0');
 if (num1 == 8 && num2 == 9)
 	continue;
 putchar(',');
-putvhar(' ');
+putchar(' ');
 }
 }
 
