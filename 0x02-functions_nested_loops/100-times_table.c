ba10c70d7dddf8d5d70688b6668754f43b6c98d2
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-13 16:57:24 -0700

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that print times table
+ * @num: input
+ * Return: Always 0 (Success);
+ */
+
+void print_times_table(int num)
+{
+	return (0);
+}
