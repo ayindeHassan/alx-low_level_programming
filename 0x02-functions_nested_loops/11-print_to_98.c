83d2b0cba8a0b264edeb55f0b1804bf974fda2e8
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-13 16:36:45 -0700

    up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,13 @@ void print_to_98(int num)
 {
 	if (num >= 98)
 	{
-		while (n > 98)
+		while (num > 98)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
-	while (n < 98)
+	while (num < 98)
 		printf("%d, ", n++);
 	printf("%d\n", n);
 	}
