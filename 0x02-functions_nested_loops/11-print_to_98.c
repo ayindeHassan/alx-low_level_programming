a5eede32890a3b4230e620838b9fcad19efb6bba
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-13 16:34:20 -0700

    numbers up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that prints up to 98
+ * @num: the number to begin counting
+ */
+
+void print_to_98(int num)
+{
+	if (num >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+	while (n < 98)
+		printf("%d, ", n++);
+	printf("%d\n", n);
+	}
+}
