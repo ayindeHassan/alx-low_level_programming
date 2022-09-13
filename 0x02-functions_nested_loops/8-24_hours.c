a1e41df47f8606b08be8a11e51c9b54e17852038
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-13 15:41:10 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 		{
 		hrs_remainder = hrs % 10;
 		mins_remainder = mins % 10;
-		_putchar(hrs % 10 + '0');
+		_putchar(hrs / 10 + '0');
 		_putchar(hrs_remainder + '0');
 		_putchar(':');
 		_putchar(mins / 10 + '0');
