34231fdc9dba491fdf26876bc678d32e1bbde578
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-13 17:26:45 -0700

    times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int num1, int num2);
 void print_to_98(int num);
-void print_times_table(int num);
+void print_times_table(int n);
 
 #endif /* MAIN_H */
