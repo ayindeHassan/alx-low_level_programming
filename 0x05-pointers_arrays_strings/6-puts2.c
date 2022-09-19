460b2b76f2dfad50c4bc4e4964c53f2b91c0b8a7
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-19 11:06:48 -0700

    printing the even number of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that print in twos i.e evn number
+ * @str: value of the string
+ */
+
+void puts2(char *str)
+{
+int count = 0;
+
+while (str[count] != '\0')
+{
+	if (str[count] % 2 == 0)
+		_putchar(str[count]);
+
+	count++;
+}
+_putchar('/n');
+}
