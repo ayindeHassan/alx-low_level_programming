682b2655d183015c0cea3a9e01d822bf3c2f8889
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-19 08:17:31 -0700

    swapping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swap the value of the argument
+ * @a: first value
+ * @b: second value
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
