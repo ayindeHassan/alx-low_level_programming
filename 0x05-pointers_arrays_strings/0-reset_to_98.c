9bba2a2c0556469f30b6734489b99deeabf6f8e4
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-19 08:00:46 -0700

    dereferencing a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that update a pointer to 98
+ *
+ * @n: A pointer argument
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+task about pointers, arrays and strings
