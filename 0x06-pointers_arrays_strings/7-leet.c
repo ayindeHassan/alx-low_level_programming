5aea3fc9f20309c010b28ecde394629a006313c9
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-09-21 16:31:26 -0700

    ecoding charater into a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - function that encode a string into numbers
+ * @s: pointer variable
+ * Return string of cahracters
+ */
+
+char *leet(char *s)
+{
+int i = 0;
+
+int j;
+
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+
+while (s[i] != '\0')
+{
+	j = 0;
+
+	while (numbers[j] != '\0')
+	{
+		if (s[i] == letters[j])
+		{
+		s[i] = numbers[j];
+		}
+		j++;
+	}
+	i++;
+}
+return (s);
+}
