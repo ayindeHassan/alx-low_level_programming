8abbe4e5d81be107ec5b81eca56758834ff6d9fb
hassan toheeb <hassanoheebayinde@gmail.com>
2022-10-20 13:03:39 +0100

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+ #include <string.h>
+ #include <stdio.h>
+
+
+ /**
+  * struct list_s - my structure
+  * @str: char * parameter
+  * @len: len
+  * @next: next node
+  */
+ typedef struct list_s
+ {
+ char *str;
+ unsigned int len;
+ struct list_s *next;
+ } list_t;
+
 int _putchar(char);
 size_t print_list(const list_t *h);
 
