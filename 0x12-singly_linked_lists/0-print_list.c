71913d3913c7cdadb8195dbfa7df337cf79f5cd8
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-10-23 22:26:51 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,22 @@
-#include "lists.h" 
-  
- /** 
-  * print_list - Write to stdout the value of list_t 
-  * @h: list_t argument 
-  * Return: number of nodes 
-  */ 
- size_t print_list(const list_t *h) 
- { 
-         size_t counter = 0; 
-  
-         /** once h-> is == NULL is false*/ 
-         while (h != NULL) 
-         { 
-                 /** checking if str is null */ 
-                 if (h->str == NULL) 
-                 { 
-                         printf("[0] (nil)\n"); 
-                         /** move to next node */ 
-                         h = h->next; 
-                 } 
-                 else 
-                 { 
-                         printf("[%d] %s\n", h->len, h->str); 
-                         /** move to next node */ 
-                         h = h->next; 
-                 } 
-                 /** store the number of nodes  */ 
-                 counter++; 
-         } 
-         return (counter); 
- }
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: list to print
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
